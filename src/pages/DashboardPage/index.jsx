2abfa66a2c5b89c4c7e54c516c7a31713ea5a4bf
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-15 18:42:58 -0300

    feat: dashboard header and profile infos

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,3 +1,64 @@
+import { useContext, useState } from "react";
+import styles from "./style.module.scss";
+import { RxHamburgerMenu, RxChevronDown } from "react-icons/rx";
+import { TbLogout, TbUserEdit } from "react-icons/tb";
+import { UserContext } from "../../providers/UserContext";
+
 export const DashboardPage = () => {
-  return <h1>DashBoardPage</h1>;
+  const [hiddenOptions, setHiddenOptions] = useState(true);
+
+  const { user } = useContext(UserContext);
+  return (
+    <>
+      <header>
+        <div className="container dash">
+          <div className={styles.headerBox}>
+            <h1 className="title bold primary">MyContacts</h1>
+            <div className={styles.profileBox}>
+              <div className={styles.firstChar}>
+                <p>{user.fullName.charAt(0).toUpperCase()}</p>
+              </div>
+              <div className={styles.buttonContainer}>
+                <button
+                  onClick={() => setHiddenOptions(!hiddenOptions)}
+                  className={styles.button}
+                >
+                  {hiddenOptions ? (
+                    <RxHamburgerMenu size={20} color="#212529" />
+                  ) : (
+                    <RxChevronDown size={20} color="#212529" />
+                  )}
+                </button>
+                {!hiddenOptions && (
+                  <div className={styles.options}>
+                    <button>
+                      <TbUserEdit size={18} color="#212529" />
+                      <p className="text">editar</p>
+                    </button>
+                    <button>
+                      <TbLogout size={18} color="#212529" />
+                      <p className="text">sair</p>
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className={styles.infosBox}>
+          <div className="container dash">
+            <div className={styles.infosContent}>
+              <p className="title bold">Olá, {user.fullName}</p>
+              <div>
+                <p className="text medium gray300">{user.email}</p>
+                <p className="text info gray300">{user.phone}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
