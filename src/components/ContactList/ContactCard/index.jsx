228f0b324ea9f5440ab88afcca01a4a8cd1920bc
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-18 13:55:10 -0300

    feat: implementing contacts values

--- src/components/ContactList/ContactCard/index.jsx
+++ src/components/ContactList/ContactCard/index.jsx
@@ -1,29 +1,36 @@
+import { useContext } from "react";
 import styles from "./style.module.scss";
 import { RxTrash, RxPencil1 } from "react-icons/rx";
+import { ContactContext } from "../../../providers/ContactContext";
 
-export const ContactCard = () => {
+export const ContactCard = ({ contact }) => {
+  const { contactDelete } = useContext(ContactContext);
   return (
     <li>
       <div className={styles.cardBox}>
         <div className={styles.headerBox}>
           <div className={styles.firstChar}>
-            <p>M</p>
+            <p>{contact.fullName.charAt(0).toUpperCase()}</p>
           </div>
           <div className={styles.buttonBox}>
             <button>
               <RxPencil1 size={16} color="#212529" />
             </button>
-            <button>
+            <button
+              onClick={() => {
+                contactDelete(contact.id);
+              }}
+            >
               <RxTrash size={16} color="#212529" />
             </button>
           </div>
         </div>
         <div className={styles.profileBox}>
           <div className={styles.nameBox}>
-            <p className="title label">Maria Gabriella Souza Lopes Silva</p>
+            <p className="title label">{contact.fullName}</p>
           </div>
-          <p className="text medium gray300">mgabislsilva@gmail.com</p>
-          <p className="text info gray300">12991709688</p>
+          <p className="text medium gray300">{contact.email}</p>
+          <p className="text info gray300">{contact.phone}</p>
         </div>
         <div></div>
       </div>
